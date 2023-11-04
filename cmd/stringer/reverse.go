162b17e8868707e955f485184dbf2491a017825e
David Baker <dpbaker1298@gmail.com>
2023-11-04 13:31:30 -0400

    Add my project

---
+++ cmd/stringer/reverse.go
@@ -0,0 +1,24 @@
+package stringer
+
+import (
+	"github.com/spf13/cobra"
+
+	"fmt"
+	"github.com/Dbaker1298/stringer/pkg/stringer"
+
+	)
+
+var reverseCmd = &cobra.Command{
+	Use: "reverse",
+	Aliases: []string{"rev"},
+	Short: "Reverses a string",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		res := stringer.Reverse(args[0])
+		fmt.Println(res)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(reverseCmd)
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/Dbaker1298/stringer/cmd/stringer"
+
+func main()  {
+	stringer.Execute()
+}
+
